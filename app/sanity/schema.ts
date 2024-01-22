91b87f22a3fcdf04b581c3f179dbb05d493611b2
Lucas Cardoso <lucascardosopsd@gmail.com>
2024-01-22 19:29:45 -0300

    schema: title

--- app/sanity/schema.ts
+++ app/sanity/schema.ts
@@ -1,5 +1,6 @@
-import { type SchemaTypeDefinition } from 'sanity'
+import { type SchemaTypeDefinition } from "sanity";
+import title from "./schemas/title";
 
 export const schema: { types: SchemaTypeDefinition[] } = {
-  types: [],
-}
+  types: [title],
+};
---
+++ app/sanity/schemas/title.ts
@@ -0,0 +1,22 @@
+export default {
+  name: "title",
+  type: "document",
+  title: "Título",
+  fields: [
+    {
+      name: "title",
+      type: "string",
+      title: "Título",
+    },
+    {
+      name: "subTitle",
+      type: "string",
+      title: "Sub Título",
+    },
+    {
+      name: "description",
+      type: "string",
+      title: "Descrição",
+    },
+  ],
+};
