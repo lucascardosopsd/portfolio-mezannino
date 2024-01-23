2491e474103f0ff47a661f2af115e87b5b818982
Lucas Cardoso <lucascardosopsd@gmail.com>
2024-01-23 02:00:35 -0300

    layout: employees cards

--- app/(pages)/(home)/page.tsx
+++ app/(pages)/(home)/page.tsx
@@ -1,12 +1,14 @@
+import { GetEmployees } from "@/actions/GetEmployees";
 import { GetTitle } from "@/actions/GetTitle";
 import MainWrapper from "@/components/MainWrapper";
 
 export default async function Home() {
   const title = await GetTitle();
+  const employees = (await GetEmployees()).sort((a, b) => a.order - b.order);
 
   return (
-    <main className="relative">
-      <MainWrapper title={title[0]} />
+    <main className="relative overflow-hidden">
+      <MainWrapper title={title[0]} employees={employees} />
     </main>
   );
 }
--- app/components/MainWrapper.tsx
+++ app/components/MainWrapper.tsx
@@ -1,10 +1,10 @@
 "use client";
-
 import { navbarLinks } from "@/constant/navbarLinks";
 import { Tabs, TabsList, TabsTrigger } from "./ui/tabs";
 import { Separator } from "./ui/separator";
-import { useTheme } from "next-themes";
 import Image from "next/image";
+import { EmployeeProps } from "@/types/employee";
+import { useTheme } from "next-themes";
 
 interface TitleProps {
   title: string;
@@ -14,47 +14,75 @@ interface TitleProps {
 
 interface MainWrapperProps {
   title: TitleProps;
+  employees: EmployeeProps[];
 }
 
-const MainWrapper = ({ title }: MainWrapperProps) => {
+const MainWrapper = ({ title, employees }: MainWrapperProps) => {
   const { theme } = useTheme();
 
   return (
-    <div className="flex flex-col items-center justify-center min-h-[90svh] gap-4 relative">
-      <Image
-        alt="grid"
-        src={theme == "light" ? "/grid-light.svg" : "/grid-dark.svg"}
-        sizes="100vh"
-        width={0}
-        height={0}
-        className="absolute h-full w-auto -z-10 m-auto bottom-0 top-0 right-0 left-0 animate-pulse"
-      />
+    <>
+      <div className="flex flex-col items-center justify-start min-h-[92svh] relative overflow-hidden gap-2">
+        <Image
+          alt="grid"
+          src={theme == "light" ? "/grid-light.svg" : "/grid-dark.svg"}
+          sizes="100vh"
+          width={0}
+          height={0}
+          className="absolute h-full w-auto -z-10 m-auto bottom-0 top-0 right-0 left-0 animate-pulse"
+        />
+        <div className="flex flex-col items-center justify-center">
+          <p className="text-sm">{title.title}</p>
+          <p className="text-4xl uppercase">{title.subTitle}</p>
+          <p className="max-w-[600px] text-center">{title.description}</p>
+        </div>
 
-      <div className="flex flex-col items-center justify-center">
-        <p className="text-lg">{title.title}</p>
-        <p className="text-6xl uppercase">{title.subTitle}</p>
-        <p className="text-xl max-w-[700px] text-center">{title.description}</p>
-      </div>
+        <Tabs defaultValue={navbarLinks[0].label} className="w-full">
+          <TabsList className="flex flex-grow gap-2 bg-transparent">
+            {navbarLinks.map((link, index) => (
+              <>
+                <TabsTrigger
+                  value={link.label}
+                  key={index}
+                  className="data-[state=active]:text-red"
+                >
+                  {link.title}
+                </TabsTrigger>
+                {index < navbarLinks.length - 1 && (
+                  <Separator orientation="vertical" />
+                )}
+              </>
+            ))}
+          </TabsList>
+        </Tabs>
 
-      <Tabs defaultValue={navbarLinks[0].label} className="w-full">
-        <TabsList className="flex flex-grow gap-2 bg-transparent">
-          {navbarLinks.map((link, index) => (
-            <>
-              <TabsTrigger
-                value={link.label}
-                key={index}
-                className="data-[state=active]:text-red"
-              >
-                {link.title}
-              </TabsTrigger>
-              {index < navbarLinks.length - 1 && (
-                <Separator orientation="vertical" />
-              )}
-            </>
+        <div className="flex gap-8 h-full">
+          {employees.map((employee, index) => (
+            <div
+              className={`relative h-[60svh] w-[300px] border border-red ${
+                index % 2 == 0 && "mt-10"
+              }`}
+              key={index}
+            >
+              <Image
+                alt="Funcionário"
+                src={employee.pic}
+                sizes="100vh"
+                width={0}
+                height={0}
+                className="object-cover h-full w-full"
+              />
+
+              <div className="absolute flex items-end justify-center text-xl pb-5 bottom-0 left-0 h-[200px] w-full bg-gradient-to-t from-black to-transparent z-10">
+                {employee.name}
+              </div>
+
+              <div className="h-full w-full -z-10 bg-gradient-to-t absolute left-0 top-0 from-red from-30% to-transparent opacity-60" />
+            </div>
           ))}
-        </TabsList>
-      </Tabs>
-    </div>
+        </div>
+      </div>
+    </>
   );
 };
 
