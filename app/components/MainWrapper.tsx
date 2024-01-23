cd3e2427bd8c16335682b5e61f78e3c15e4c7a99
Lucas Cardoso <lucascardosopsd@gmail.com>
2024-01-22 23:00:43 -0300

    layout: navbar

--- app/components/MainWrapper.tsx
+++ app/components/MainWrapper.tsx
@@ -1,3 +1,7 @@
+import { navbarLinks } from "@/constant/navbarLinks";
+import { Tabs, TabsList, TabsTrigger } from "./ui/tabs";
+import { Separator } from "./ui/separator";
+
 interface TitleProps {
   title: string;
   subTitle: string;
@@ -10,10 +14,31 @@ interface MainWrapperProps {
 
 const MainWrapper = ({ title }: MainWrapperProps) => {
   return (
-    <div className="flex flex-col items-center justify-center min-h-[90svh]">
-      <p className="text-lg">{title.title}</p>
-      <p className="text-6xl uppercase">{title.subTitle}</p>
-      <p className="text-xl max-w-[700px] text-center">{title.description}</p>
+    <div className="flex flex-col items-center justify-center min-h-[90svh] gap-4">
+      <div className="flex flex-col items-center justify-center">
+        <p className="text-lg">{title.title}</p>
+        <p className="text-6xl uppercase">{title.subTitle}</p>
+        <p className="text-xl max-w-[700px] text-center">{title.description}</p>
+      </div>
+
+      <Tabs defaultValue={navbarLinks[0].label} className="w-full">
+        <TabsList className="flex flex-grow gap-2 bg-transparent">
+          {navbarLinks.map((link, index) => (
+            <>
+              <TabsTrigger
+                value={link.label}
+                key={index}
+                className="data-[state=active]:text-red"
+              >
+                {link.title}
+              </TabsTrigger>
+              {index < navbarLinks.length - 1 && (
+                <Separator orientation="vertical" />
+              )}
+            </>
+          ))}
+        </TabsList>
+      </Tabs>
     </div>
   );
 };
---
+++ app/constant/navbarLinks.ts
@@ -0,0 +1,26 @@
+export const navbarLinks = [
+  {
+    label: "home",
+    title: "Início",
+  },
+  {
+    label: "portfolio",
+    title: "Projetos",
+  },
+  {
+    label: "services",
+    title: "Serviços",
+  },
+  {
+    label: "employees",
+    title: "Profissionais",
+  },
+  {
+    label: "clients",
+    title: "Clientes",
+  },
+  {
+    label: "about",
+    title: "Sobre",
+  },
+];
