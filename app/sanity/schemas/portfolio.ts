c2546b13de9469c94b1d8e417d9606cee48771d7
Lucas Cardoso <lucascardosopsd@gmail.com>
2024-01-23 13:20:39 -0300

    schema: portfolio

---
+++ app/sanity/schemas/portfolio.ts
@@ -0,0 +1,22 @@
+export default {
+  name: "portfolio",
+  type: "document",
+  title: "Projetos",
+  fields: [
+    {
+      name: "order",
+      type: "number",
+      title: "Posição",
+    },
+    {
+      name: "title",
+      type: "string",
+      title: "Título",
+    },
+    {
+      name: "description",
+      type: "text",
+      title: "Descrição",
+    },
+  ],
+};
