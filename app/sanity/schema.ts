75a3fabbccc0def938962689dd2c9f1b50c41277
Lucas Cardoso <lucascardosopsd@gmail.com>
2024-01-23 18:26:10 -0300

    schema: service

--- app/sanity/schema.ts
+++ app/sanity/schema.ts
@@ -2,7 +2,8 @@ import { type SchemaTypeDefinition } from "sanity";
 import title from "./schemas/title";
 import employee from "./schemas/employee";
 import portfolio from "./schemas/portfolio";
+import service from "./schemas/service";
 
 export const schema: { types: SchemaTypeDefinition[] } = {
-  types: [title, employee, portfolio],
+  types: [title, employee, portfolio, service],
 };
---
+++ app/sanity/schemas/service.ts
@@ -0,0 +1,22 @@
+export default {
+  name: "services",
+  type: "document",
+  title: "Serviços",
+  fields: [
+    {
+      name: "order",
+      type: "number",
+      title: "Posição",
+    },
+    {
+      name: "title",
+      type: "string",
+      title: "Título",
+    },
+    {
+      name: "image",
+      type: "image",
+      title: "Foto",
+    },
+  ],
+};
