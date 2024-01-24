63f86161ae273b0462298379fb8f78a88a970770
Lucas Cardoso <lucascardosopsd@gmail.com>
2024-01-24 00:53:28 -0300

    fix: open-graph image url

--- app/layout.tsx
+++ app/layout.tsx
@@ -18,12 +18,12 @@ export const metadata: Metadata = {
     siteName: "MEZANNINO | Engenharia",
     images: [
       {
-        url: "/open-graph.svg",
+        url: "/open-graph.png",
         width: 800,
         height: 600,
       },
       {
-        url: "/open-graph.svg",
+        url: "/open-graph.png",
         width: 1800,
         height: 1600,
         alt: "MEZANNINO Engenharia",
