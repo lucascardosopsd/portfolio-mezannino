cc01a315b84cb0854b314379fed7b3bb4650f138
Lucas Cardoso <lucascardosopsd@gmail.com>
2024-01-24 18:20:47 -0300

    fix: WppFloatingButton visibility

--- app/components/WppFloatingButton.tsx
+++ app/components/WppFloatingButton.tsx
@@ -3,12 +3,12 @@ import { PiWhatsappLogoThin } from "react-icons/pi";
 
 const WppFloatingButton = () => {
   return (
-    <div className="fixed z-20 bottom-4 right-4">
+    <div className="fixed z-20 bottom-4 right-4 block tablet:hidden">
       <Link
         href="https://api.whatsapp.com/send?phone=5517992777857"
         className="relative"
       >
-        <div className="flex items-center justify-center border border-red bg-background text-red rounded-full h-16 w-16 tablet:hidden">
+        <div className="flex items-center justify-center border border-red bg-background text-red rounded-full h-16 w-16">
           <PiWhatsappLogoThin className="text-2xl" />
         </div>
 
