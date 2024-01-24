11a782a9a9d7a0384f903a6c354406839a817639
Lucas Cardoso <lucascardosopsd@gmail.com>
2024-01-23 21:25:56 -0300

    layout: scroll smooth

--- app/layout.tsx
+++ app/layout.tsx
@@ -21,7 +21,7 @@ export default function RootLayout({
   children: React.ReactNode;
 }>) {
   return (
-    <html lang="pt-BR">
+    <html lang="pt-BR" className="!scroll-smooth">
       <body className={poppins.className}>
         <ThemeProvider
           attribute="class"
