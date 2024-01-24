5dbe7337fb8f86e6587e91b9f3d83611ba23fa44
Lucas Cardoso <lucascardosopsd@gmail.com>
2024-01-24 16:57:38 -0300

    refact: remove floating contact button from layout container

--- app/(pages)/(home)/layout.tsx
+++ app/(pages)/(home)/layout.tsx
@@ -7,28 +7,29 @@ import { ReactNode } from "react";
 
 export default function Layout({ children }: { children: ReactNode }) {
   return (
-    <div className="container">
+    <>
       <WppFloatButton />
+      <div className="container">
+        <Image
+          alt="grid"
+          src="/grid-dark.svg"
+          sizes="100vh"
+          width={0}
+          height={0}
+          className="fixed h-[80vh] w-[80vw] -z-10 m-auto bottom-0 top-0 right-0 left-0 animate-pulse hidden dark:block"
+        />
 
-      <Image
-        alt="grid"
-        src="/grid-dark.svg"
-        sizes="100vh"
-        width={0}
-        height={0}
-        className="fixed h-[80vh] w-[80vw] -z-10 m-auto bottom-0 top-0 right-0 left-0 animate-pulse hidden dark:block"
-      />
-
-      <Image
-        alt="grid"
-        src="/grid-light.svg"
-        sizes="100vh"
-        width={0}
-        height={0}
-        className="fixed h-[80vh] w-[80vw] -z-10 m-auto bottom-0 top-0 right-0 left-0 animate-pulse block dark:hidden"
-      />
-      <Navbar />
-      {children}
-    </div>
+        <Image
+          alt="grid"
+          src="/grid-light.svg"
+          sizes="100vh"
+          width={0}
+          height={0}
+          className="fixed h-[80vh] w-[80vw] -z-10 m-auto bottom-0 top-0 right-0 left-0 animate-pulse block dark:hidden"
+        />
+        <Navbar />
+        {children}
+      </div>
+    </>
   );
 }
